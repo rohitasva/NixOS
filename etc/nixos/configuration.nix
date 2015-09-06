da5eabd8e02f94ce5eff707b69e81d934ccc5f76
rohitasva <rohitasva.rohitasva@lookout.com>
2015-09-05 17:38:09 -0700

    Initial Commit of nixos configuration file

---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,88 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the gummiboot efi boot loader.
+  boot.loader.gummiboot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    jre = pkgs.oraclejre8;
+    jdk = pkgs.oraclejdk8;
+  };
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    wget vim zsh nix-prefetch-scripts
+  ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+
+  # Enable the KDE Desktop Environment.
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "mac";
+    xkbOptions = "eurosign:e";
+    displayManager.kdm = {
+      enable = true;
+    };
+    desktopManager.kde4.enable = true;
+    videoDrivers = [ "intel" "nouveau" ];
+  
+    # Configure touchpad
+    synaptics = {
+      enable = true;
+      dev = "/dev/input/event*";
+      twoFingerScroll = true;
+      accelFactor = "0.0001";
+      buttonsMap = [ 1 3 2 ];
+    };
+  };
+  
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = true;
+  users.extraUsers.rrohitasva = {
+    isNormalUser = true;
+    uid = 1000;
+    group  = "users";
+    description = "Home Account";
+    extraGroups = [ "wheel" ];
+    home = "/home/rrohitasva";
+  };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "15.09";
+
+}
